842430a28e79fc68685b8312e0ad05567218aaa5
PasinduTY <pasindu.t.y.s@gmail.com>
2026-02-19 20:31:22 +0530

    Add PUT and DELETE endpoints to BooksController

--- backend/BookManagementAPI/BookManagementAPI/Controllers/BooksController.cs
+++ backend/BookManagementAPI/BookManagementAPI/Controllers/BooksController.cs
@@ -34,5 +34,38 @@ namespace BookManagementAPI.Controllers
 
             return CreatedAtAction(nameof(GetBooks), new { id = book.Id }, book);
         }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, Book updatedBook)
+        {
+            var existingBook = books.FirstOrDefault(b => b.Id == id);
+
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+
+            existingBook.Title = updatedBook.Title;
+            existingBook.Author = updatedBook.Author;
+            existingBook.Isbn = updatedBook.Isbn;
+            existingBook.PublicationDate = updatedBook.PublicationDate;
+
+            return Ok(existingBook);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            books.Remove(book);
+
+            return NoContent();
+        }
     }
 }
