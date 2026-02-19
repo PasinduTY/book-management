db123dc5e090a62b671a718bcc61f6e291aded21
PasinduTY <pasindu.t.y.s@gmail.com>
2026-02-19 19:47:41 +0530

    Add BooksController with GET endpoint

---
+++ backend/BookManagementAPI/BookManagementAPI/Controllers/BooksController.cs
@@ -0,0 +1,29 @@
+﻿using BookManagementAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private static List<Book> books = new List<Book>
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Harry Potter",
+                Author = "J.K. Rowling",
+                Isbn = "12345",
+                PublicationDate = DateTime.Now
+            }
+        };
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok(books);
+        }
+    }
+}
